d0506ace167a3ed82fd303fb596521112e512b9a
Anil <anilfth2001@gmail.com>
2023-02-14 15:08:05 +0530

    intial commit

--- 14feb.js
+++ 14feb.js
@@ -51,7 +51,15 @@
 //  {
 //   console.log("good evening");
 //  }
-let a= 4;
-let b = 5;
-let c = a+b;
-console.log(c);
+// let a= 4;
+// let b = 5;
+// let c = a+b;
+// console.log(c);
+
+// while loops in javascript/
+
+let i;
+while(i < 10){
+  console.log(i);
+  i++;
+}
