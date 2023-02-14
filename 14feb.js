1b63ba4179862b2e40b0a3dd972c65238d4cdc5f
Anil <anilfth2001@gmail.com>
2023-02-14 10:41:57 +0530

    intial commit

--- 14feb.js
+++ 14feb.js
@@ -1,33 +1,39 @@
- // switch statement explanation
- const gadget = 'laptop'
-switch(gadget){
-    case 'laptop':
-        console.log("laptop price is 900000")
-        break;
-        case 'smartphone':
-            console.log("smartphone price is 80000")
-            break;
-            case 'charger':
-                console.log("charger price is 1200")
-                break;
-                default:
-                    console.log(`sorry ! ${gadget} is not available`)
-                    break;
+//  // switch statement explanation
+//  const gadget = 'laptop'
+// switch(gadget){
+//     case 'laptop':
+//         console.log("laptop price is 900000")
+//         break;
+//         case 'smartphone':
+//             console.log("smartphone price is 80000")
+//             break;
+//             case 'charger':
+//                 console.log("charger price is 1200")
+//                 break;
+//                 default:
+//                     console.log(`sorry ! ${gadget} is not available`)
+//                     break;
 
-}
- // switch vs dictonary object
- let city = 'India'
-  switch(city){
-    case 'india':
-        console.log('hi india')
-        break;
-        case 'America':
-            console.log('Hi America')
-            break;
-            case 'China':
-                console.log('Hi china')
-                break;
-                default:
-                    console.log('welcome !')
-                    break;
-  }
+// }
+//  // switch vs dictonary object
+//  let city = 'India'
+//   switch(city){
+//     case 'india':
+//         console.log('hi india')
+//         break;
+//         case 'America':
+//             console.log('Hi America')
+//             break;
+//             case 'China':
+//                 console.log('Hi china')
+//                 break;
+//                 default:
+//                     console.log('welcome !')
+//                     break;
+//   }
+
+                        // for loops
+  let i =0;
+  for(;i<10;i++){
+    console.log(i)
+  }                      
