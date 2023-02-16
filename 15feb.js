de5e979f813fadb197757973641b81d97944567b
Anil <anilfth2001@gmail.com>
2023-02-16 10:59:33 +0530

    intial commit

--- 15feb.js
+++ 15feb.js
@@ -1,4 +1,23 @@
+function printMassage(){
+    document.getElementById("para1").innerHTML += "We are learning basic javascript in inner HTML";
+    // document.getElementById("para1").innerHTML = "nothing";
+    // if we are using class then 
+    document.getElementsByClassName("p1").innerHTML += "We are learning basic javascript in inner HTML";
+
+
+    document.getElementById("para1").style.color = "red";
+    document.getElementById("para1").style.border = "2px solid black";
+
+}
+ // div function
+ function formatDiv(){
+    document.getElementById("btn2").classList.add("drawdiv");
+    document.getElementById("btn1").classList.add("drawdiv");
+
+ }
+
 function createTable(){
+    document.getElementById("table").style.color = "pink";
     const tb1 = document.createElement("table");
     const tbd = document.createElement("tbody");
     for(let i=0;i<5;i++){  // creating rows
